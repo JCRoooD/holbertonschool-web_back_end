a2c39c28ca4cefc61d786656f72f01426793acb5
JCRoooD <juancarlos-99@live.com>
2024-01-03 15:14:42 -0400

    fix type annotation

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,9 @@
 #!/bin/usr/env python3
 """basic annotations - sum_list"""
 
+from typing import List
 
-def sum_list(input_list: float) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """returns the sum of a list of floats"""
     return sum(input_list)
