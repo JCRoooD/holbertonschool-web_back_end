554208d2ec50688a1cccd9b3586bc872d71e92c6
JCRoooD <juancarlos-99@live.com>
2024-01-03 20:18:11 -0400

    fixed pycodetyle

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """duck typing - element length"""
 from typing import Iterable, Sequence, Tuple, List
 
-def element_length(lst : Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """returns list of element lengths"""
     return [(i, len(i)) for i in lst]
