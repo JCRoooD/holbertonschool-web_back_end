cbe584f50cfdeacee5f40056b23d3020c8685429
JCRoooD <juancarlos-99@live.com>
2024-01-05 10:23:47 -0400

    fixed pycodestyle

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,10 +1,7 @@
 #!/usr/bin/env python3
 """async comprehension module"""
-
-async_generator = __import__('0-async_generator').async_generator
 from typing import List
-import asyncio
-import random
+async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
