3b8df40ebc18c33da4f8beef0151244335dafd0b
JCRoooD <juancarlos-99@live.com>
2024-01-25 14:28:59 -0400

    promise return

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,13 +1,10 @@
 function getFullResponseFromAPI(success){
   return new Promise((resolve, reject) => {
-    if (success) {
-      console.log('Got a response from the API');
+    if (success){
       resolve({ status: 200, body: 'Success' })
     } else {
-      console.error('The fake API is not working currently')
-      reject(new Error());
+      reject('The fake API is not working currently')
     }
   });
 }
-
 export default getFullResponseFromAPI;
