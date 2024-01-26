93f1f3de9f93f02bcfbae2dd7e839908805ae59c
JCRoooD <juancarlos-99@live.com>
2024-01-26 14:34:25 -0400

    utilizing catch

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,7 +1,12 @@
 function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'success' }))
-    .catch(() => Error())
-    .finally(() => console.log('Got a response from the API'));
+  return promise
+  .then(() => {
+    console.log('Got a response from the API');
+    return { status: 200, body: 'success' };
+  })
+  .catch(() => {
+    console.error('An error occurred');
+    return new Error();
+  });
 }
-
 export default handleResponseFromAPI;
